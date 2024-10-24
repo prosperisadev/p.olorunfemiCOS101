331e3ff55f600cd171e8194c8370e6d60afb3173
prosperisadev <olorunfemiprosperity@gmail.com>
2024-10-24 16:15:27 +0100

    (My second commit)

---
+++ week-2/practice_2.rs
@@ -0,0 +1,10 @@
+fn main() {
+	println!(); //prints just a
+
+	println!("hello ");//prints
+
+	println!("format {} arguments", "some"); //prints format some arguments
+
+	println!("My name is {}. I am not the class rep of stream 1", "Prosperity Olorunfemi-Olaoluwa");
+
+}
