f7086850b39aa7fb454e782bff04f1cc40d9fa8f
prosperisadev <olorunfemiprosperity@gmail.com>
2025-01-05 16:58:25 +0100

    (My Last Commit)

--- week-8/project_1/src/main.rs
+++ week-8/project_1/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to Public Service APS leel checker by FGN, input your information to check your level\n");
+
+    let level = vec!["Intern", "Paralegal", "Placement", "Administrator", "Research Assistant", "Junior Associate", "Classroom Teacher", "Senior Administrator",
+    "PhD Candidate", "Associate", "Snr Teacher", "Office Manager", "Post-Doc Researcher", "Senior Associate 1-2", "Leading Teacher", "Director", "Senior Lecturer",
+    "Senior Associate 3-4", "Deputy Principal", "CEO", "Dean", "Partner", "Principal"]
+
+    let mut input1 = String::new();
+    println!("Input your level as a staff i.e intern, paralegal, administrator or CEO...");
+    std::io::stdin().read_line(&mut input1).expect("Invalid String");
+    let staff_level: String = input1.trim().parse().expect("Not a valid staff level");
+
+    let mut input2 = String::new();
+    println!("Input no. of years of experience i.e 1,2,3,4...");
+    std::io::stdin().read_line(&mut input2).expect("Invalid String");
+    let years_of_experience:usize = input2.trim().parse().expect("Not a valid number");
+
+
+    let v = Vec
+
 }
---
+++ week-9/practice_2/target/debug/practice_2.d
@@ -0,0 +1,1 @@
+C:\Users\ZBOOK\ 15\ G5\Documents\p.olorunfemiCOS101\week-9\practice_2\target\debug\practice_2.exe: C:\Users\ZBOOK\ 15\ G5\Documents\p.olorunfemiCOS101\week-9\practice_2\src\main.rs
